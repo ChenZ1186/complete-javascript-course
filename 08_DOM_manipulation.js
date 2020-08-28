95d6b6dc0e3a506f569b62a0e8fd326b3dc6ddab
ChenZ1186 <67185975+ChenZ1186@users.noreply.github.com>
2020-08-28 20:16:01 +0300

    DOM js and html

---
+++ 08_DOM_manipulation.js
@@ -0,0 +1,29 @@
+/*                  DOM Manipulation              */
+
+/* till now we only talked about the Language itself. but never about how we can actually use javascript in the browser to make it interact with the webpage.
+we gonna build our first small project together. the project is a fun game which we can play in the browser. so it's basically just an HTML webpage that is gonna track with the javascript code that we'll be writing.
+
+
+DOM: * Document Object Model
+<body>
+    <section>
+        <p> A paragraph with a <a>link</a>.</p>
+        <p> Another second paragraph.</p>
+    </section>
+
+    <section>
+      <img src ="x.jpg" alt="The DOM">
+    </section>
+</body>
+    * Structured representation of an HTML dodument;
+    * The DOM is used to connect webpages to scripts like JavaScript or programming Languages such as JavaScript.
+    * For each HTML box, there is an object in the DOM that we can access and interact with.
+    DOM is a fully object-oriented representation. so for each of these boxes, there is an object in the DOM.
+
+    JAVASCRIPT AND DOM ARE TWO DIFFERENT THINGS!!
+
+    JAVASCRIPT <--interaction / Manipulation --> DOM
+
+    we gonna use methods on webpages and since we are talking on methods that means that we will use functions attached to some object and that object is the document object. so this is the object that gives us access to the DOM.
+    the HTML webpage content is stored in the DOM, which can then be accessed and manipulation by JavaScript.
+                  */
